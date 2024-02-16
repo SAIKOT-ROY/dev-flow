d79042b2bcf46f0ea3b0644c721c16a11fc21b14
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-02-16 18:11:28 +0600

    theme is added for desktop

---
+++ constants/index.ts
@@ -0,0 +1,50 @@
+import moon from "@/public/assets/icons/moon.svg"
+import sun from "@/public/assets/icons/sun.svg"
+import system from "@/public/assets/icons/computer.svg"
+import { SidebarLink } from "@/types";
+
+export const themes = [
+    { value: "light", label: 'Light', icon: sun },
+    { value: "dark", label: 'Dark', icon: moon },
+    { value: "system", label: 'System', icon: system }
+]
+
+
+export const sidebarLinks: SidebarLink[] = [
+    {
+        imgURL: "/assets/icons/home.svg",
+        route: "/",
+        label: "Home",
+    },
+    {
+        imgURL: "/assets/icons/users.svg",
+        route: "/community",
+        label: "Community",
+    },
+    {
+        imgURL: "/assets/icons/star.svg",
+        route: "/collection",
+        label: "Collections",
+    },
+    {
+        imgURL: "/assets/icons/suitcase.svg",
+        route: "/jobs",
+        label: "Find Jobs",
+    },
+    {
+        imgURL: "/assets/icons/tag.svg",
+        route: "/tags",
+        label: "Tags",
+    },
+    {
+        imgURL: "/assets/icons/user.svg",
+        route: "/profile",
+        label: "Profile",
+    },
+    {
+        imgURL: "/assets/icons/question.svg",
+        route: "/ask-question",
+        label: "Ask a question",
+    },
+];
+
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -6,7 +6,7 @@ interface ThemeContextType {
     mode: string;
     setMode: (mode: string) => void;
 }
- const ThemeContext =  createContext<ThemeContextType | undefined>(undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: {
     children: React.ReactNode
@@ -14,22 +14,22 @@ export function ThemeProvider({ children }: {
     const [mode, setMode] = useState('');
 
     const handleThemeChange = () => {
-        if (mode === 'dark') {
-            setMode('light');
-            document.documentElement.classList.add('light');
-        } else {
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia("(prefers-color-scheme:dark)").matches)) {
             setMode('dark');
             document.documentElement.classList.add('dark');
+        } else {
+            setMode('light');
+            document.documentElement.classList.remove('dark');
         }
     }
 
     useEffect(() => {
         handleThemeChange()
-    }, [])
+    }, [mode])
 
     return (
         <ThemeContext.Provider value={
-            {mode,setMode}
+            { mode, setMode }
         }>
             {children}
         </ThemeContext.Provider>
