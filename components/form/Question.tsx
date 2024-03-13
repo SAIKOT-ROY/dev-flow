d35b45685cc9bb71bda0010a875e7e4fe809d8a7
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-03-13 16:28:27 +0600

    the Editor is created for the Ask a Question Page

--- components/form/Question.tsx
+++ components/form/Question.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -19,6 +21,14 @@ import { QuestionsSchema } from "@/lib/validations"
 
 
 const Question = () => {
+    const editorRef = useRef(null);
+
+    // const log = () => {
+    //     if (editorRef.current) {
+    //       console.log(editorRef.current.getContent());
+    //     }
+    //   };
+
     const form = useForm<z.infer<typeof QuestionsSchema>>({
         resolver: zodResolver(QuestionsSchema),
         defaultValues: {
@@ -45,8 +55,8 @@ const Question = () => {
                             <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
                                 <Input
-                                 className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                                 {...field} />
+                                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                                    {...field} />
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Be specific and imagine you&apos;re asking question to another person.
@@ -62,7 +72,28 @@ const Question = () => {
                         <FormItem className="flex w-full flex-col gap-3">
                             <FormLabel className="paragraph-semibold text-dark400_light800">Detailed Explanation of your problem<span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
-                                {/* TODO: ADD and Editor Components */}
+                                <Editor
+                                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                                    onInit={(evt, editor) =>
+                                        // @ts-ignore
+                                        editorRef.current = editor
+                                    }
+                                    initialValue=""
+                                    init={{
+                                        height: 350,
+                                        menubar: false,
+                                        plugins: [
+                                            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
+                                            'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
+                                            'insertdatetime', 'media', 'table'
+                                        ]
+                                        ,
+                                        toolbar: 'undo redo |' +
+                                            'codesample | bold italic forecolor | alignleft aligncenter ' +
+                                            'alignright alignjustify | bullist numlist',
+                                        content_style: 'body { font-family:Inter; font-size:16px }'
+                                    }}
+                                />
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Introduced the problem and expand on what you put in the title. Minimum 20 characters.
@@ -80,8 +111,8 @@ const Question = () => {
                                 Tags <span className="text-primary-500">*</span></FormLabel>
                             <FormControl className="mt-3.5">
                                 <Input
-                                 className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                                 {...field} placeholder="Add Tags..." />
+                                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                                    {...field} placeholder="Add Tags..." />
                             </FormControl>
                             <FormDescription className="body-regular mt-2.5 text-light-500">
                                 Add up to 3 tags to describe what your question is about. You need to press enter to add a tag.
