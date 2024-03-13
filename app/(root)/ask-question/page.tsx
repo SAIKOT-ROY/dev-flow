2a06feedc64e2485d60ae736987371b52037dce5
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-03-13 13:14:26 +0600

    Ask question page the input field were created but not the editor

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import Question from '@/components/form/Question';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            <h1>Ask A question</h1>
+            <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
+            <div className='mt-9'>
+                <Question />
+            </div>
         </div>
     );
 };
