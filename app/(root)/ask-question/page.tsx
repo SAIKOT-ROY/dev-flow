73c61d6c16a7028ea8819241eeaabe21333400d0
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-05-02 11:43:54 +0600

    remove auth from the ask-question page that was never used

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,5 @@
 import Question from '@/components/form/Question';
 import { getUserById } from '@/lib/actions/user.action';
-import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
