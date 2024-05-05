f9a6211b71ef7a7fed2d7b3df62503325082a556
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-05-05 20:11:35 +0600

    add public route to middleware this time 100% sure

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,16 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: [
         '/',
+        'api/webhook',
+        '/question/:id',
+        '/tags',
+        '/tags/:id',
+        '/profile/:id',
+        '/community',
+        '/jobs'
+    ],
+    ignoredRoutes: [
+        '/api/webhook', '/api/chatgpt'
     ]
 });
 
